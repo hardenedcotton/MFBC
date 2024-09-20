154976dfc98f513718808f6ac26f06d03b1056e5
hardenedcotton <onursener4@gmail.com>
2024-09-20 06:23:14 +0300

    added autorun for train.py

---
+++ autorun.py
@@ -0,0 +1,14 @@
+import sys
+import os
+"""
+Used for running the training multiple times. Replace `x` with repetition value.
+
+`py autorun.py x` 
+"""
+x = 1  # Default to 1 iteration
+
+if (len(sys.argv) > 1):
+    x = int(sys.argv[1])
+
+for i in range(0, x):
+    os.system("py -3.8 train.py")
